b46980073cb6e645bb8f61cee9cccc8df72c13d8
Jonathan Dunlap <jonnyd2885@gmail.com>
2021-05-09 22:10:01 +0700

    Create MVC files for decisions route

---
+++ api/app/models/decisions.js
@@ -0,0 +1,12 @@
+module.exports = [
+  {
+    id: '4dca50fb-a048-41e8-bc69-4c3faf06abaf',
+    title: 'What is for lunch?',
+    type: 'public',
+  },
+  {
+    id: '633206f2-7365-4370-9366-1d23b98bc02a',
+    title: 'What should we do tonight?',
+    type: 'private',
+  },
+];
