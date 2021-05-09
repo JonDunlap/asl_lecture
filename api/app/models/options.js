3988b9a807148f612cd7f4a49d20b0987d789105
Jonathan Dunlap <jonnyd2885@gmail.com>
2021-05-09 22:11:04 +0700

    Create MVC files for options route

---
+++ api/app/models/options.js
@@ -0,0 +1,32 @@
+module.exports = [
+  {
+    id: 'sby30u7x-1sbt-3e10-58pe-z7ha4yxmu247',
+    value: 'Babbleopia',
+    decisionId: 'ldy74a9p-0rxz-6l63-05tc-r3mt0jmci745',
+  },
+  {
+    id: 'fpf36h4p-3nak-9d49-38hg-q2bv5vmyg469',
+    value: 'Plambee',
+    decisionId: 'lvr10k7r-4myt-4r68-84ys-r2hf5kgsh650',
+  },
+  {
+    id: 'fas96b5z-1mce-5q44-49rz-n0fn4oiwy178',
+    value: 'Trilia',
+    decisionId: 'axp22a7p-5cnv-0q88-69zf-o8rw9ngtb551',
+  },
+  {
+    id: 'iwp63v2d-7riw-2w48-33bt-h8fa0udub556',
+    value: 'Quatz',
+    decisionId: 'eqj50a9c-6ors-4s92-69vx-s2df1auea025',
+  },
+  {
+    id: 'hbc57b4u-3oej-5b74-93iw-z0qh1axzo929',
+    value: 'Blogspan',
+    decisionId: 'qxi08z3p-0lnr-5u17-83fy-z5gl9pbij528',
+  },
+  {
+    id: 'dag39p3f-8gaz-4f94-89br-v8gq8ieoc513',
+    value: 'Topiczoom',
+    decisionId: 'tkd57k8l-3ntu-8i93-60ls-f9qi5wibo334',
+  },
+];
