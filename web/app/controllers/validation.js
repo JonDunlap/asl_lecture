f813ec7ac25496e0eb05b0f9c73f9da5cf593977
Jonathan Dunlap <jonnyd2885@gmail.com>
2021-05-12 15:13:48 +0700

    Create validation controller

---
+++ web/app/controllers/validation.js
@@ -0,0 +1,38 @@
+const { check, validationResult } = require('express-validator');
+
+const checks = {
+  title: check('title')
+    .exists()
+    .withMessage('Decision title is required.')
+    .isLength(3)
+    .withMessage(
+      'Decision title is required to be at least 3 characters long.'
+    ),
+  type: check('type')
+    .exists()
+    .withMessage('Decision type is required.')
+    .isIn(['public', 'private'])
+    .withMessage('Decisions must be public or private.'),
+};
+
+const checkForErrors = (req, res, next) => {
+  // get any errors
+  const errors = validationResult(req);
+
+  // if there are errors go to the next error handler middleware with the errors from the validation
+  if (!errors.isEmpty()) return next(errors.mapped());
+  // if there are NO errors, go to the next middleware function
+  return next();
+};
+
+exports.validate = (method) => {
+  switch (method) {
+    case 'createDecisions': {
+      return [checks.title, checks.type, checkForErrors];
+    }
+
+    default: {
+      return [];
+    }
+  }
+};
