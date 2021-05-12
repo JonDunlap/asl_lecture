9d45d6e91ffe8f22a03d2bce49dd00fe18e8f5c5
Jonathan Dunlap <jonnyd2885@gmail.com>
2021-05-12 15:14:45 +0700

    Add route to validate POST from the user

--- web/app/routes/adminDecisions.js
+++ web/app/routes/adminDecisions.js
@@ -3,9 +3,12 @@ const router = require('express').Router();
 
 // load teh controller
 const decisionCtrl = require('../controllers/decisions');
+const validationCtrl = require('../controllers/validation');
 
 // GET /admin/decisions/new - loads the form to create a new decision
 router.get('/new', decisionCtrl.renderDecisionForm);
+// POST /admin/decisions/new - validate the data and then save it
+router.post('/new', validationCtrl.validate('createDecisions'));
 
 // export the route from this file
 module.exports = router;
