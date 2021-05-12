e98637f428f8dccdf2c4430723f75a6712a29f8f
Jonathan Dunlap <jonnyd2885@gmail.com>
2021-05-12 13:27:37 +0700

    Update the app index.js

--- web/app/index.js
+++ web/app/index.js
@@ -1,32 +1,12 @@
 // pull in the express package
 const express = require('express');
 // add the logger
-const log = require('debug')('web:logging');
-// add another logger
 const error = require('debug')('web:error');
 // create an express app
 const app = express();
+
 // setup a folder to hold all the static files
 app.use(express.static('public'));
-// example middleware that runs once for every request
-app.use(
-  (req, res, next) => {
-    log('\nRUNS ONCE FOR EVERY REQUEST');
-    setTimeout(() => {
-      next();
-    }, 2000);
-  },
-  (req, res, next) => {
-    log('WILL RUN WHEN NEXT IS CALLED');
-    next();
-  },
-);
-
-// route specific middleware
-app.use('/about', (req, res, next) => {
-  log('RUNS ONLY ON the /about page');
-  next(new Error('Not authorized'));
-});
 
 // four params are required to mark this as an error handling middleware
 // the comment below allows for eslint to not throw an error
@@ -34,7 +14,7 @@ app.use('/about', (req, res, next) => {
 // eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
   error('ERROR FOUND:', err);
-  res.sendStatus(5000);
+  res.sendStatus(500);
 });
 
 // export the express app
