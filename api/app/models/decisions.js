15af264b53fab6df95cbffd8708bf54de269f11c
Jonathan Dunlap <jonnyd2885@gmail.com>
2021-05-17 13:27:33 +0700

    Add validation for decision values

--- api/app/models/decisions.js
+++ api/app/models/decisions.js
@@ -6,9 +6,30 @@ module.exports = (sequelize, DataTypes) => {
         defaultValue: DataTypes.UUIDV4,
         primaryKey: true,
         type: DataTypes.UUID,
+        validate: {
+          isUUID: { args: 4, msg: 'Id not valid, please try again' },
+        },
       },
-      title: DataTypes.STRING,
-      type: DataTypes.ENUM('public', 'private'),
+
+      title: {
+        type: DataTypes.STRING,
+        validate: {
+          len: {
+            args: [3, 500],
+            msg: 'Decisions title is required to be at least 3 characters',
+          },
+        },
+      },
+
+      type: {
+        type: DataTypes.ENUM('public', 'private'),
+        validate: {
+          isIn: {
+            args: [['public', 'private']],
+            msg: 'Decisions must be either public or private',
+          },
+        },
+      },
     },
     {}
   );
