622fe5407478a8b9a06c366605a1935ff6a74f15
Jonathan Dunlap <jonnyd2885@gmail.com>
2021-05-20 15:38:45 +0700

    Add route for login

--- web/app/routes/public.js
+++ web/app/routes/public.js
@@ -3,9 +3,12 @@ const router = require('express').Router();
 
 // load the controller
 const decisionCtrl = require('../controllers/decisions');
+const authCtrl = require('../controllers/auth');
 
 // GET / - loads the home page
 router.get('/', decisionCtrl.renderLanding);
+// GET /login - loads the login page
+router.get('/login', authCtrl.renderLogin);
 
 // export the router
 module.exports = router;
