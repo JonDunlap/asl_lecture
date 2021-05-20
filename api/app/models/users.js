b56a46f914fd7072af4ceb0c79d7b844469f0bba
Jonathan Dunlap <jonnyd2885@gmail.com>
2021-05-20 16:22:43 +0700

    Create users model with datatypes and associations

---
+++ api/app/models/users.js
@@ -0,0 +1,57 @@
+module.exports = (sequelize, DataTypes) => {
+  const Users = sequelize.define(
+    'Users',
+    {
+      id: {
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        type: DataTypes.UUID,
+        validate: {
+          isUUID: {
+            args: 4,
+            msg: 'Id not valid, please go back and try again.',
+          },
+        },
+      },
+
+      username: {
+        type: DataTypes.STRING,
+        unique: { args: true, msg: 'Username is already in use.' },
+        allowNull: { args: false, msg: 'Username is required.' },
+      },
+
+      access_token: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+
+      name: {
+        type: DataTypes.STRING,
+        allowNull: { args: false, msg: 'Name is required.' },
+      },
+
+      password: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+
+      type: {
+        type: DataTypes.ENUM('slack', 'regular'),
+        validate: {
+          isIn: {
+            args: [['slack', 'regular']],
+            msg: 'User type must be Slack or regular.',
+          },
+        },
+      },
+    },
+    {}
+  );
+
+  Users.associate = (models) => {
+    // define association here
+    Users.hasMany(models.Decisions, { foreignKey: 'userId' });
+  };
+
+  return Users;
+};
