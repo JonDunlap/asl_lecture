bebb5e51e92f499cc59eb979625598a79fa5d998
Jonathan Dunlap <jonnyd2885@gmail.com>
2021-05-20 15:55:49 +0700

    Add routes for authentication to the API

--- api/app/index.js
+++ api/app/index.js
@@ -2,19 +2,27 @@
 const error = require('debug')('api:error');
 const express = require('express');
 const morganDebug = require('morgan-debug');
+
 // routes
 const decisionRouter = require('./routes/decisions');
 const optionRouter = require('./routes/options');
+const authRouter = require('./routes/auth');
+
 // create an express application
 const app = express();
+
 // checks to see if the content-type is json and parses it into req.body
 app.use(express.json());
 // log all requests
 app.use(morganDebug('api:request', 'dev'));
+
 // setup the app to use the router at /decisions
 app.use('/decisions', decisionRouter);
 // setup the app to use the router at /options
 app.use('/options', optionRouter);
+// setup the app to use the router at /auth
+app.use('/auth', authRouter);
+
 // four params are required to mark this as a error handling middleware
 // eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
