78e8e679e13e6d2131372238bc0919922eddd59a
Jonathan Dunlap <jonnyd2885@gmail.com>
2021-05-27 18:20:21 +0700

    Add ignore to remove errors preventing app from running

--- reactjs/.eslintrc.js
+++ reactjs/.eslintrc.js
@@ -3,5 +3,7 @@ module.exports = {
   rules: {
     // turning off old rule of needing jsx in file names
     'react/jsx-filename-extension': 0,
+    'react/jsx-props-no-spreading': 'off',
+    'react/state-in-constructor': 'off',
   },
 };
