1e9d1b976485c587a4158f0caf73c6ac86c2d57a
Jonathan Dunlap <jonnyd2885@gmail.com>
2021-05-27 18:21:11 +0700

    Create HOC container for authentication from Slack

---
+++ reactjs/src/containers/auth.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import API from '../API';
+
+export default function container(Component) {
+  class AuthContainer extends React.Component {
+    state = {
+      loggedIn: !!localStorage.getItem('token'),
+    };
+
+    logout = () => {
+      localStorage.removeItem('token');
+      this.setState({ loggedIn: false });
+    };
+
+    verifySlackCode = async (code) => {
+      const { token, loggedIn } = await API.post('/auth/slack', {
+        code,
+        url: process.env.REACT_APP_CALLBACK_URL,
+      });
+      localStorage.setItem('token', token);
+      this.setState({ loggedIn });
+    };
+
+    render() {
+      const { loggedIn } = this.state;
+
+      return (
+        <Component
+          /* Pass all other props that are being passed to this component forward */
+          {...this.props}
+          loggedIn={loggedIn}
+          logout={this.logout}
+          verifySlackCode={this.verifySlackCode}
+        />
+      );
+    }
+  }
+  return AuthContainer;
+}
