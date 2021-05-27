b56b9cdce1fd95bab6b86f9340c3f4767b063675
Jonathan Dunlap <jonnyd2885@gmail.com>
2021-05-27 17:53:17 +0700

    Add logout function w/ proptypes

--- reactjs/src/header/index.js
+++ reactjs/src/header/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import RRPropTypes from 'react-router-prop-types';
 import { Link } from 'react-router-dom';
 import styles from './styles.module.css';
 import AuthContainer from '../containers/auth';
 
 class Header extends React.Component {
-  logUserOut = () => {};
+  logUserOut = () => {
+    const { logout, history } = this.props;
+    logout();
+    history.push('/');
+  };
 
   render() {
     const { loggedIn } = this.props;
@@ -51,6 +56,8 @@ class Header extends React.Component {
 
 Header.propTypes = {
   loggedIn: PropTypes.bool,
+  logout: PropTypes.func.isRequired,
+  history: RRPropTypes.history.isRequired,
 };
 
 Header.defaultProps = {
