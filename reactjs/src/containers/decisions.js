f4b3fa566f3f0d6a05a17220adcb73705a171893
Jonathan Dunlap <jonnyd2885@gmail.com>
2021-05-27 18:48:52 +0700

    Add props

--- reactjs/src/containers/decisions.js
+++ reactjs/src/containers/decisions.js
@@ -1,15 +1,30 @@
 import React from 'react';
+import API from '../API';
 
 export default function container(Component) {
   return class DecisionsContainer extends React.Component {
     // the default state
-    state = {};
+    state = {
+      userDecisions: [],
+    };
+
+    // get this user's decisions
+    fetchUserDecisions = async () => {
+      // get the user decisions from the API
+      const userDecisions = await API.get('/decisions');
+      // update the state
+      this.setState({ userDecisions });
+    };
 
     render() {
+      const { userDecisions } = this.state;
+
       return (
         <Component
           /* pass all other props that are being passed to this component forward */
           {...this.props}
+          userDecisions={userDecisions}
+          fetchUserDecisions={this.fetchUserDecisions}
         />
       );
     }
