cbd2f714c97badcb722a19cbdc2dfa774592d451
Jonathan Dunlap <jonnyd2885@gmail.com>
2021-05-27 18:00:19 +0700

    Add request interceptor to set headers if there is a token

--- reactjs/src/API.js
+++ reactjs/src/API.js
@@ -11,4 +11,17 @@ API.interceptors.response.use(
   }
 );
 
+// for each API request going out
+API.interceptors.request.use(async (config) => {
+  // pull the token out of the local storage
+  const token = localStorage.getItem('token');
+  // if there is no token, do nothing
+  if (!token) return config;
+  // if there is a token, set a header for any request that contains the token
+  return {
+    ...config,
+    headers: { common: { token } },
+  };
+});
+
 export default API;
