c20cc84504f58a4cf90cf8fabdab7c58cc5e066b
Jonathan Dunlap <jonnyd2885@gmail.com>
2021-05-27 12:00:43 +0700

    Add prettier to prevent eslint warnings

--- reactjs/.eslintrc.js
+++ reactjs/.eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['airbnb', 'react-app'],
+  extends: ['airbnb', 'react-app', 'prettier'],
   rules: {
     // turning off old rule of needing jsx in file names
     'react/jsx-filename-extension': 0,
